5463f0d7d6b7712b9255d95382e1d56640fab43b
tugenhua <tugenhua@tugenhuadeMacBook-Pro.local>
2017-10-11 16:30:03 +0800

    update

--- src/components/table-column/table-column.vue
+++ src/components/table-column/table-column.vue
@@ -7,6 +7,7 @@
 
 <script>
   import Emitter from '../../mixins/emitter';
+  import Vue from 'vue';
   export default {
     mixins: [ Emitter ],
     name: 'TbTableColumn',
@@ -22,13 +23,27 @@
       
     },
     mounted() {
-      
+      this.$nextTick(() => {
+        this.compile();
+      });
     },
     created() {
 
     },
     methods: {
-      
+      compile () {
+        var self = this;
+        console.log(this)
+        const component = new Vue({
+          functional: true,
+          render: (h) => {
+            console.log(h)
+            //return this.column.render(h, this.row, this.column, this.index);
+          }
+        });
+        //const Cell = component.$mount();
+        //this.$refs.cell.appendChild(Cell.$el);
+      }
     }
   }
 </script>
